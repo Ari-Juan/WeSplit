b2dedb355b743081139f85f435c578e93854708a
Ari-Juan <juanj.ariza.r@gmail.com>
2025-11-10 14:55:18 -0600

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Juan Ariza on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
